3c009e1f58d7720fec8a3614c473530073dbe193
zoulei <zoulei@weihaizixun.com>
2021-12-08 10:06:49 +0800

    快乐数

--- 算法/002/002.js
+++ 算法/002/002.js
@@ -65,3 +65,43 @@ const hasCycle = function (head) {
   }
   return null
 };
+
+
+
+/**
+ * @todo 202.快乐数
+ * 编写一个算法来判断一个数 n 是不是快乐数。
+
+「快乐数」定义为：
+
+对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和。
+然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。
+如果 可以变为  1，那么这个数就是快乐数。
+如果 n 是快乐数就返回 true ；不是，则返回 false 。
+
+ * @param {number} n
+ * @return {boolean}
+ */
+ var isHappy = function(n) {
+  if(n <= 0 ) return false
+  let numSet = new Set()
+  let num = n
+  while(true){
+      let sum = getSum(num)
+      if(sum === 1) return true
+      if(numSet.has(sum)){
+          return false
+      }else{
+          numSet.add(sum)
+      }
+      num =sum
+  }
+
+};
+function getSum(num){
+  let numArr=[...num.toString()]
+  let sum = numArr.reduce((a,b)=>{
+      return (a*1) + (b*1)*(b*1)
+  },0)
+  return sum
+}
